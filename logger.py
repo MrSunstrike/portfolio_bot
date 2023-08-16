f97be4ad6ca846fd833f56dd94a15c1b67188f60
Михаил Никитин <misha@mrsunstrike.ru>
2023-08-16 12:51:16 +0700

    Переписал логику логгера

--- logger.py
+++ logger.py
@@ -1,15 +1,21 @@
 import logging
 from config import LOG_FORMAT
 
-def setup_logger(name):
-    logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
-    logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+file_handler = logging.FileHandler("global.log")
+file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+file_handler.setLevel(logging.INFO)
 
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+
+def setup_logger(name):
     file_handler = logging.FileHandler(name + ".log")
     file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
     file_handler.setLevel(logging.INFO)
 
-    logger = logging.getLogger()
-    logger.addHandler(file_handler)
+    local_logger = logging.getLogger(name)
+    local_logger.addHandler(file_handler)
 
-    return logger
+    return local_logger
