88e0f0ee1e03e5851bee98caba7d29cdb7ec0ed0
Михаил Никитин <misha@mrsunstrike.ru>
2023-08-16 09:30:37 +0700

    Написал функцию распознания речи из голосовых

--- utils.py
+++ utils.py
@@ -1,1 +1,18 @@
-# для вспомогательных функций
+import speech_recognition as speech_r
+from pydub import AudioSegment
+
+def speech_to_text(path):
+    # конвертирование файла .oga в .wav
+    audio = AudioSegment.from_file(path, format="ogg")
+    wav_path = path.replace(".oga", ".wav")
+    audio.export(wav_path, format="wav")
+    
+    # распознавание речи
+    sample = speech_r.AudioFile(wav_path)
+    recognizer = speech_r.Recognizer()
+
+    with sample as audio_file:
+        recognizer.adjust_for_ambient_noise(audio_file)
+        content = recognizer.record(audio_file)
+
+        return recognizer.recognize_google(content, language="ru-RU")
